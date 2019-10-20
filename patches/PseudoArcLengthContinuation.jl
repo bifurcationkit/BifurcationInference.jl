5c7d1bee31a71ff3afd5279f1751e8568d9a7136
gszep <gregory.szep@gmail.com>
2019-10-20 23:56:51 +0100

    updating unit tests to match master branch of PsuedoArclengthCont

--- patches/PseudoArcLengthContinuation.jl
+++ patches/PseudoArcLengthContinuation.jl
@@ -13,6 +13,7 @@ initial_state(curve::ContResult) = [ param(curve.branch[i,1].data) for i=2:size(
 
 # patches for Cont.continuation()
 vcat(p::TrackedReal{T}, u::Vector{TrackedReal{T}}, i::Int64, ds::TrackedReal{T}) where {T<:Real} = [ p, u..., i, ds ]
+vcat(p::TrackedReal{T}, u::TrackedReal{T}, i::Int64, ds::TrackedReal{T}) where {T<:Real} = [ p, u, i, ds ]
 round(x::TrackedReal{T}, mode::RoundingMode; kwargs...) where {T<:Real} = round(x.data, mode; kwargs...)
 
 eigen(x::Array{TrackedReal{T}}; kwargs...) where {T<:Real} = eigen( Tracker.data.(x); kwargs...)
@@ -21,7 +22,7 @@ eigen!(x::Array{TrackedReal{T}}; kwargs...) where {T<:Real} = eigen!( Tracker.da
 
 """ extension of co-dimension one parameter continuation methods to Array{TrackedReal} type """
 function continuation( f::Function, J::Function, u₀::Vector{T}, p₀::T,
-	printsolution = u->u ; kwargs...) where {T<:Number}
+	printsolution = u->u[1] ; kwargs...) where {T<:Number}
 
 	options = Cont.NewtonPar{T, typeof(DefaultLS()), typeof(DefaultEig())}(verbose=false,maxIter=kwargs[:maxIter])
 	return Cont.continuation( f,J, u₀,p₀,
