4d704fe72cc4a4b4d3e11932f8c23fd74f8336cd
gszep <gregory.szep@gmail.com>
2020-10-21 09:51:16 +0200

    central difference estimate

--- test/autodiff/leibniz.jl
+++ test/autodiff/leibniz.jl
@@ -18,7 +18,7 @@ end
 # integrand under dz
 integrand( z::AbstractVector, θ::AbstractVector ) = integrand( z[Not(end)], z[end], θ )
 function integrand( u::AbstractVector, p::Number, θ::AbstractVector )
-	return norm(θ)^2 + 3exp(-(p-1)^2/0.05) # just an example
+	return 3exp(-(p-1)^2/0.01) # just an example
 end
 
 ########################################################## gradients
@@ -39,29 +39,29 @@ function ∇region( z::AbstractVector, θ::AbstractVector )
 end
 
 ########################################################
-function cost(θ)
+function cost(θ;ds=0.01)
 	θ = typeof(θ) <: Tuple ? [θ...] : θ
-	∂S = solutions(θ)
+	∂S = solutions(θ,ds=ds)
 
-	return sum(z->integrand(z,θ),∂S) / length(∂S)
+	return sum(z->integrand(z,θ),∂S)*ds
 end
 
-function ∇cost(θ;scale=1e-3)
+function ∇cost(θ;scale=1e-3,ds=0.01)
 	θ = typeof(θ) <: Tuple ? [θ...] : θ
-	∂S = solutions(θ)
+	∂S = solutions(θ,ds=ds)
 
-	gradient = sum(z->∇integrand(z,θ),∂S) / length(∂S)
+	gradient = sum(z->∇integrand(z,θ),∂S)*ds
 	return scale * gradient / norm(gradient)
 end
 
 ########################################################### utils
-function solutions(θ)
+function solutions(θ;ds=0.01)
 	θ = typeof(θ) <: Tuple ? [θ...] : θ
 
 	# parameters for the continuation
-	opts = ContinuationPar(dsmax = 0.01, dsmin = 0.01, ds = 0.01,
+	opts = ContinuationPar(dsmax = ds, dsmin = ds, ds = ds,
 		maxSteps = 1000, pMin = 0.0, pMax = 2.0, saveSolEveryStep = 0,
-		newtonOptions = NewtonPar(tol = 1e-8,verbose = false)
+		newtonOptions = NewtonPar(tol = 1e-8, verbose = false)
 	)
 
 	# we define an iterator to hold the continuation routine
@@ -86,42 +86,42 @@ function ∂Fz( z::AbstractVector, θ::AbstractVector )
 	return ForwardDiff.jacobian( z -> F(z,θ) , z )
 end
 
+##################################
+##################################
+################################## central differences
 
-function finite_differences(x,y;scale=1e-3)
+struct OneHot <: AbstractVector{Int}
+	n::Int
+	k::Int
+end
+import Base: size,getindex
+size(x::OneHot) = (x.n,)
+getindex(x::OneHot,i::Int) = Int(x.k==i)
 
-	grid = collect(Iterators.product(x,y))
-	cost_landscape = cost.(grid)
+function central_differences(θ;scale=1e-3,Δx=1e-2)
+	θ = typeof(θ) <: Tuple ? [θ...] : θ
+	gradient = similar(θ)
 
-	Px = vcat(map( x->x[1], grid[1:end-1,1:end-1])...)
-	Py = vcat(map( x->x[2], grid[1:end-1,1:end-1])...)
+	for i ∈ 1:length(θ)
+		Δθ = Δx*OneHot(length(θ),i)
+		gradient[i] = ( cost(θ+Δθ)-cost(θ-Δθ) )/(2Δx)
+	end
 
-	Vx = vcat(diff(cost_landscape, dims=1)[:,1:end-1]...)
-	Vy = vcat(diff(cost_landscape, dims=2)[1:end-1,:]...)
-
-	Nx = map( (x,y)->scale*x/norm([x,y]), Vx,Vy)
-	Ny = map( (x,y)->scale*y/norm([x,y]), Vx,Vy)
-
-	return Px,Py,Nx,Ny
+	return scale * gradient / norm(gradient)
 end
 
-###################################
-###################################
-###################################
-
-######################### finite difference gradient estimate
-x,y = range(1.4,2,length=31), range(0.1,0.4,length=29)
-Px,Py,Vx,Vy = finite_differences(x,y)
-
 ##################################
 ##################################
-################################## plot it all
+##################################
 
+x,y = range(1.4,2,length=31), range(0.4,0.6,length=29)
 plot(size=(600,600), xlabel="parameters, θ")
+
 contourf!( x, y, (x,y)->cost((x,y)) )
 plot!( x, maximum(y)*ones(length(x)), label="", fillrange=minimum(y), color=:white, alpha=0.5 )
 
-quiver!( x[1:end-1], y[1:end-1]', quiver=(x,y)->∇cost((x,y)), color=:darkblue, lw=3)
-quiver!( Px, Py, quiver=(Vx,Vy), lw=2, color=:gold)
+quiver!( x, y',  quiver=(x,y)->∇cost((x,y)), color=:darkblue, lw=3)
+quiver!(  x, y', quiver=(x,y)->central_differences((x,y)), color=:gold, lw=2)
 
 plot!([],[],color=:darkblue, lw=3, label="ForwardDiff")
-plot!([],[],color=:gold, lw=3, label="Finite Differences")
+plot!([],[],color=:gold, lw=3, label="Central Differences")
